a86a5d7e0647f1dfe918d41823aefbf26277e732
lynndixon <lynn@redhat.com>
2025-06-19 23:01:45 -0400

    L2 Arguments

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from stats import get_num_words, get_chars_dict, sort_chars, format_chars
+import sys
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    try:
+        book_path = get_book_path()
+    except Exception as e:
+        print(f"{e}")
+        sys.exit(1)
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -19,5 +24,12 @@ def main():
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_book_path():
+    if len(sys.argv) != 2:
+        raise Exception("Usage: python3 main.py <path_to_book>")
+    return sys.argv[1]
+
+
 
 main()
